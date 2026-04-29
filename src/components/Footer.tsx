75bc8a152a2ba34af1ca83938e30f4d668da63c5
xuanpv89 <166982083+xuanpv89@users.noreply.github.com>
2026-04-30 03:58:58 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,9 +15,9 @@ export const Footer: FunctionComponent = () => {
         </div>
         <div className="text-xs text-muted-foreground hidden lg:block">
           <Link
-            href={`https://phamdaibang.com/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
+            href={`https://folksteam.com${config.baseUrl}`}
           >
-            Blog powered by Folks
+            Blog powered by FOLKS
           </Link>
         </div>
         <div>
