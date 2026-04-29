e8ad2a00b6e2bc0042cc932a2ccf9d5108644ded
xuanpv89 <166982083+xuanpv89@users.noreply.github.com>
2026-04-30 03:13:56 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ export const Footer: FunctionComponent = () => {
         <Link
           href={`https://wisp.blog/?utm_source=next-js-template&utm_medium=web&utm_campaign=${config.baseUrl}`}
         >
-          Blog powered by wisp
+          Blog powered by Folks
         </Link>
       </div>
     </section>
